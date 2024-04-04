deac7a663d9b75d44b5ded9c2cb4f7b74c178c3d
Koen <koennenoen@gmail.com>
2024-04-04 11:05:28 +0200

    Simpel database stuff

--- invoegen.php
+++ invoegen.php
@@ -12,8 +12,23 @@
         <a href="index.html"><button>Home</button></a>
         <a href="invoegen.php"><button>Invoegen</button></a>
         <a href="overzicht.php"><button>Overzicht</button></a>
-        <form>
-        </form>
+        <h2>Hout per m³ invullen</h2>
+        <?php
+            include 'db.php'; 
+            $sql = "SELECT * FROM `hout`"; 
+            $result = mysqli_query($con, $sql ); 
+
+            echo '<form action = "toevoegen.php" method= "POST">';
+            while($row = mysqli_fetch_assoc($result)) {
+                echo $row['name']."<br>";
+                echo $row['price']."<br>";
+                echo $row['lengteCM']."<br>";
+                echo $row['dikteMM']."<br>";
+                echo $row['bijzonderheden']."<br>";
+            
+            }
+            echo '<input type="submit" value="Bestellen"></form>';
+        ?>
     </div>
 </body>
 </html>
